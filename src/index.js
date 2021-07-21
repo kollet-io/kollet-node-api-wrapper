fb0d3ba8e8b8603e6f5b8816249bf36a893e34fc
kevinchrist20 <kevinamevor@outlook.com>
2021-07-21 15:54:43 +0000

    Added errorCodes with error messages and implemented createAddress method

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Kollet {
     #baseUrl = "https://api.kollet.io/v1/";
     #endpoints = {
         currencies: "currencies",
-        address: "address/create",
+        create_address: "address/create",
         balance: "balance",
         estimate_fee: "estimateFee",
         send: "send"
@@ -22,38 +22,69 @@ class Kollet {
      */
     constructor(apiKey) {
         this.apiKey = apiKey;
-    }
+    };
 
-    #request(endpoint = "", options = {}) {
+    _request(endpoint = "", options = {}) {
         let url  = `${this.#baseUrl}${endpoint}`;
         
-        let headers = {
+        let header = {
             "Content-Type": "application/json"
         };
 
         let config = {
             ...options,
-            ...headers
+            headers: header
         };
 
         return fetch(url, config).then(res => {
-            if(res.ok) {
-                return res.json()
+            if (res.ok) {
+                return res.json();
             }
-            throw new Error(res)
+            throw new Error(res);
         })
     }
 
     /**
-     * Get all the supported cryptocurrencies by Kollet.
+     * Get all the cryptocurrencies supported by Kollet.
      */
     getCurrencies() {
         let url = this.#endpoints.currencies;
+        let payload = {
+            accessToken: this.apiKey
+        }
         let config = {
-            method: "POST",
-            body: JSON.stringify(this.apiKey)
+            method: "post",
+            body: JSON.stringify(payload)
         };
 
-        return this.#request(url, config);
+        return this._request(url, config);
+    }
+
+    /**
+     * Generate a payment address. This is the address that customers pay to.
+     * @param {string} currency This is the code of the supported cryptocurrency. 
+     * Visit the supported cryptocurrency section to view the currencies supported and their various codes. e.g. BTC
+     * @param {string} label This is a unique id that is linked to the address. 
+     * You can use this id to identify different users or payment addresses.
+     * @param {object} metadata An optional field where you can store a JSON object. 
+     * This field is attached to all webhooks when we are notifying you of new changes to the status of a payment.
+     */
+    createAddress(currency = "", label = "", metadata = {}) {
+        let url = this.#endpoints.create_address;
+        let payload = {
+            accessToken: this.apiKey,
+            currency,
+            label,
+            metadata
+        };
+
+        let config = {
+            method: "post",
+            body: JSON.stringify(payload)
+        };
+
+        return this._request(url, config);
     }
 }
+
+module.exports = Kollet
